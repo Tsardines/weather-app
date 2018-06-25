b49283569c4d6e089d4e7962ab119ecdca72f734
Haley Sulcer <hjsulcer@gmail.com>
2018-06-24 23:07:20 -0400

    created more components

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'; // import React from the 'react package' that's in package.json
 
 import Titles from './components/Titles';
+import Form from './components/Form';
+import Weather from './components/Weather';
 
 class App extends React.Component { // creates an instance of App that extends React.Component (an object that exists somewhere in node_modules)
   render() {
     return (
       <div>
         <Titles />
+        <Form />
+        <Weather />
       </div>
     );
   }
