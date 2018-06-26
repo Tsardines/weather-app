f61f5774ee1c5798d6364d8d4bb077efa7ced3c1
Haley Sulcer <hjsulcer@gmail.com>
2018-06-25 23:28:06 -0400

    added props and state

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -7,16 +7,49 @@ import Weather from './components/Weather';
 const API_KEY = "28f1851bcf3dff53a03ebef5540086ff";
 
 class App extends React.Component {
-  getWeather = async () => {
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}&units=imperial`);
+
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+
+  getWeather = async (e) => {
+    e.preventDefault(); // prevent default behavior of component (prevents full page refresh)
+
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`);
+    const data = await api_call.json(); // converts API call to json format
+    console.log(data);
+
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ""
+    });
   }
 
   render() {
     return (
       <div>
         <Titles />
-        <Form />
-        <Weather />
+        <Form getWeather={this.getWeather} />
+        <Weather
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+          />
       </div>
     );
   }
--- weather-app/src/components/Form.js
+++ weather-app/src/components/Form.js
@@ -3,9 +3,17 @@ import React from 'react';
 class Form extends React.Component {
   render() {
     return (
-      <div>Form Component</div>
+      <form onSubmit={this.props.getWeather}>
+        <input type="text" name="city" placeholder="City..."/>
+        <input type="text" name="country" placeholder="Country..."/>
+        <button>Get Weather</button>
+      </form>
     );
   }
 };
 
 export default Form;
+
+
+
+// the 'this' on line 6 references the 'this' in App.js (see <Form />)
