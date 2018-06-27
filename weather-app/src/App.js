1dc13bb8e457dfe9801e958c7feb3eceb0771fdb
Haley Sulcer <hjsulcer@gmail.com>
2018-06-26 22:53:12 -0400

    condensed code

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -25,16 +25,26 @@ class App extends React.Component {
 
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`);
     const data = await api_call.json(); // converts API call to json format
-    console.log(data);
 
-    this.setState({
-      temperature: data.main.temp,
-      city: data.name,
-      country: data.sys.country,
-      humidity: data.main.humidity,
-      description: data.weather[0].description,
-      error: ""
-    });
+    if (city && country) { // if city & country are present, code will run
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter your information."
+      });
+    }
   }
 
   render() {
